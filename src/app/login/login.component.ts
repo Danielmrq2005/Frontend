02193acc7fb99f195193e299fe1d403ec68ebe94
Davidmb1622 <davidmarin162@gmail.com>
2025-01-22 14:17:35 +0100

    holacaracola

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,19 +1,48 @@
 import { Component, OnInit } from '@angular/core';
-import {IonicModule} from "@ionic/angular";
-
+import { IonicModule } from "@ionic/angular";
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
   standalone: true,
   imports: [
-    IonicModule
+    IonicModule,
+    FormsModule
   ]
 })
-export class LoginComponent  implements OnInit {
-
-  constructor() { }
+export class LoginComponent implements OnInit {
+  username: string = '';
+  password: string = '';
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {}
 
+  async handleLogin() {
+    const loginData = {
+      nombreUsuario: this.username,
+      contraseña: this.password,
+    };
+
+    try {
+      const response = await this.http
+        .post('http://localhost:8080/auth/login', loginData, { observe: 'response' })
+        .toPromise();
+
+      if (response?.status === 200) {
+        alert('Login exitoso');
+      }
+    } catch (error: any) {
+      const errorMessage = error.error?.mensaje || 'Ocurrió un error';
+      const container = document.querySelector('.login-container') as HTMLElement;
+
+      if (container) {
+        const errorElement = document.createElement('p');
+        errorElement.textContent = errorMessage;
+        errorElement.style.color = 'red';
+        container.appendChild(errorElement);
+      }
+    }
+  }
 }
--- src/app/registro/registro.component.ts
+++ src/app/registro/registro.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import {IonicModule} from "@ionic/angular";
 
 @Component({
-  selector: 'app-registro',
-  templateUrl: './registro.component.html',
-  styleUrls: ['./registro.component.scss'],
-  standalone: true,
+    selector: 'app-registro',
+    templateUrl: './registro.component.html',
+    styleUrls: ['./registro.component.scss'],
+    standalone: true,
+    imports: [
+        IonicModule
+    ]
 })
 export class RegistroComponent  implements OnInit {
 
