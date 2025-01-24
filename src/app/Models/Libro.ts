fad6253b745b8eb0b1c13a34e0e1e85486482aa0
dam <dmartinrodriguezdequesada@safareyes.es>
2025-01-24 13:14:08 +0100

    funcionalidad de ver libros

---
+++ src/app/Models/Libro.ts
@@ -0,0 +1,10 @@
+export interface Libro {
+  id: number;
+  nombre: string;
+  descripcion: string;
+  generos: string;
+  imagen: string;
+  autorId: number;
+  likes: number;
+  dislikes: number;
+}
---
+++ src/app/Services/LibroService.ts
@@ -0,0 +1,33 @@
+import {Injectable} from "@angular/core";
+import {HttpClient, HttpParams} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Libro} from "../Models/Libro";
+
+@Injectable({
+  providedIn: 'root',
+})
+
+export class LibroService {
+  private apiUrl = 'http://localhost:8080/libros';
+
+
+  constructor(private http: HttpClient) {
+  }
+
+
+  listarlibros(): Observable<Libro[]> {
+    return this.http.get<Libro[]>(this.apiUrl);
+  }
+
+  publicarlibro(libro: Partial<Libro>): Observable<Libro> {
+    return this.http.post<Libro>(`${this.apiUrl}/crear`, libro);
+  }
+
+  votarlibro(libroId: number, usuarioId: number, esLike: boolean): Observable<any> {
+    const params = new HttpParams()
+      .set('usuarioId', usuarioId.toString())
+      .set('esLike', esLike.toString());
+
+    return this.http.post(`${this.apiUrl}/votar/${libroId}`, {}, {params});
+  }
+}
