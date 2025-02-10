700ace1b6df43deb6e0edc1c253eb83c30cd9ee2
Davidmb1622 <davidmarin162@gmail.com>
2025-02-10 08:43:42 +0100

    chat en proceso 5

--- src/app/services/ChatUsuarioService.ts
+++ src/app/services/ChatUsuarioService.ts
@@ -1,6 +1,7 @@
 import {Injectable} from "@angular/core";
 import {environment} from "../../environments/environment";
 import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root',
@@ -13,7 +14,14 @@ export class ChatUsuarioService {
   constructor(private http: HttpClient) {
   }
 
-  
+  obtenerUsuariosEnChat(chatId: number): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/usuarios/${chatId}`);
+  }
+
+  obtenerChatsDeUsuario(usuarioId: number): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/chats/${usuarioId}`);
+  }
+
 
 
 
