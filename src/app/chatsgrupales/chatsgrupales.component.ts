1574487606b2c6393aba392242880fd33e40fee3
Davidmb1622 <davidmarin162@gmail.com>
2025-02-10 10:00:40 +0100

    chat en proceso 6

--- src/app/chatsgrupales/chatsgrupales.component.ts
+++ src/app/chatsgrupales/chatsgrupales.component.ts
@@ -4,6 +4,9 @@ import {HttpClientModule} from "@angular/common/http";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {CommonModule, KeyValuePipe} from "@angular/common";
 import {ChatUsuarioService} from "../Services/ChatUsuarioService";
+import {Chatusuarios} from "../modelos/Chatusuarios";
+import {finalize} from "rxjs";
+import {jwtDecode} from "jwt-decode";
 
 @Component({
   selector: 'app-chatsgrupales',
@@ -21,9 +24,56 @@ import {ChatUsuarioService} from "../Services/ChatUsuarioService";
 })
 export class ChatsgrupalesComponent  implements OnInit {
 
-  constructor() { }
+  constructor(private chatusuarioService: ChatUsuarioService) { }
 
   ngOnInit() {}
+
+
+
+
+
+
+  agregarUsuarioschat(){
+
+    const userId = this.obtenerUsuarioId();
+
+    if (!userId) {
+      console.error('No se encontró la ID del usuario');
+      return;
+    }
+
+
+
+
+
+    const nuevousuario:Chatusuarios  = {
+        usuarioId: userId,
+        chatId:chatId,
+
+      };
+
+      this.comentariosService.agregarComentario(nuevoComentario).subscribe(
+        () => {
+          console.log('Comentario agregado con éxito');
+
+          // Limpiar el input
+          this.nuevoComentario = '';
+
+          // 🚀 Volver a cargar los comentarios DESDE EL BACKEND
+          this.cargarComentarios();
+        },
+        error => console.error('Error al agregar comentario', error)
+      );
+    }
+
+  protected readonly home = home;
+  }
+
+  }
+
+  crearChat(){
+
+  }
 
 
 
