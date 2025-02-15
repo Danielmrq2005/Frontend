497c6cc355c5c1194a45da91451050a26d75cb8a
usuario <efernandezjurado@safareyes.es>
2025-02-15 18:07:48 +0100

    BEBÉ LO LOGRAMOS

--- src/app/publicaciones/publicaciones.component.ts
+++ src/app/publicaciones/publicaciones.component.ts
@@ -1,17 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HttpClient, HttpHeaders } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { add } from 'ionicons/icons';
 import { addIcons } from 'ionicons';
 import { Libro } from '../Models/Libro';
 import { LibroService } from '../Services/LibroService';
-import {Router, RouterLink} from "@angular/router";
+import { Router, RouterLink } from "@angular/router";
 import { finalize } from 'rxjs/operators';
-import {NavbarComponent} from "../navbar/navbar.component";
-import {jwtDecode} from "jwt-decode";
+import { NavbarComponent } from "../navbar/navbar.component";
+import { jwtDecode } from "jwt-decode";
 import { RouterModule } from '@angular/router';
-
 
 @Component({
   selector: 'app-publicaciones',
@@ -32,22 +31,19 @@ import { RouterModule } from '@angular/router';
 export class PublicacionesComponent implements OnInit {
   libros: Libro[] = [];
 
-  constructor(private libroService: LibroService, private router: Router) {
+  constructor(private libroService: LibroService, private router: Router, private http: HttpClient) {
     addIcons({ add });
   }
+
   verDetallesLibro(libroId: number) {
     this.router.navigate(['detallesLibro', libroId]);
   }
 
   idactual: number = this.obtenerUsuarioId();
 
-
   async ngOnInit() {
     await this.listarLibros();
   }
-
-
-
 
   listarLibros() {
     this.libroService.listarlibros().pipe(
@@ -57,7 +53,7 @@ export class PublicacionesComponent implements OnInit {
       next: (libros: Libro[]) => {
         this.libros = libros;
       },
-      error: (error) => {
+      error: (error: any) => {
         console.error('Error al obtener libros', error);
       }
     });
@@ -65,7 +61,7 @@ export class PublicacionesComponent implements OnInit {
 
 
   // @ts-ignore
-  obtenerUsuarioId(): number {
+  obtenerUsuarioId(): number{
     const token = sessionStorage.getItem('authToken');
     if (token) {
       try {
@@ -75,23 +71,43 @@ export class PublicacionesComponent implements OnInit {
         console.error('Error al decodificar el token', error);
       }
     }
+
   }
-
-
 
   votar(libroId: number, esLike: boolean) {
     const usuarioId = this.obtenerUsuarioId();
-    this.libroService.votarlibro(libroId, usuarioId, esLike).pipe(
-      finalize(() => {
-      })
-    ).subscribe({
-      next: (response) => {
-        console.log('Voto registrado con éxito');
-        this.listarLibros();
-      },
-      error: (error) => {
-        console.error('Error al registrar voto', error);
-      }
-    });
+    const url = esLike
+      ? 'http://localhost:8080/libros-favoritos/anyadirLibroFavorito'
+      : 'http://localhost:8080/libros-favoritos/eliminarLibroFav';
+    const body = { userId: usuarioId, libroId: libroId };
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+    if (esLike) {
+      this.http.post(url, body, { headers }).pipe(
+        finalize(() => {
+          this.listarLibros();
+        })
+      ).subscribe({
+        next: (response: any) => {
+          console.log('Libro añadido a favoritos con éxito');
+        },
+        error: (error: any) => {
+          console.error('Error al añadir libro a favoritos', error);
+        }
+      });
+    } else {
+      this.http.delete(url, { headers, body }).pipe(
+        finalize(() => {
+          this.listarLibros();
+        })
+      ).subscribe({
+        next: (response: any) => {
+          console.log('Libro eliminado de favoritos con éxito');
+        },
+        error: (error: any) => {
+          console.error('Error al eliminar libro de favoritos', error);
+        }
+      });
+    }
   }
 }
