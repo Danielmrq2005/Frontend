9801d405c3b0cfba83a5e120f00fcad8e7ef94ec
Moip1824 <mimouokhomepilares@safareyes.es>
2025-02-17 13:10:34 +0100

    compañeres lo de les imagenes ya este arreglade.

--- src/app/services/UsuarioService.ts
+++ src/app/services/UsuarioService.ts
@@ -2,13 +2,12 @@ import {Injectable} from "@angular/core";
 import {HttpClient, HttpParams} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Libro} from "../Models/Libro";
-import {Perfil} from "../Models/Perfil";
 
 @Injectable({
   providedIn: 'root',
 })
 export class UsuarioService {
-  private apiUrl = 'http://localhost:8081/usuario';
+  private apiUrl = 'http://localhost:8080/usuario';
   constructor(private http: HttpClient) {
   }
 
